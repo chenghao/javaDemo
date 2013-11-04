79a85d23c5489fa62e2b49e7f9e2c9c72f6a8503
chenghao <hao2432@gmail.com>
2013-11-04 14:36:21 +0800

    关闭Channel

--- src/com/hao/nio/NioCopy.java
+++ src/com/hao/nio/NioCopy.java
@@ -10,21 +10,23 @@ import java.nio.channels.FileChannel;
 public class NioCopy {
 
 	public void copy(String fromFilePath, String toFilePath) {
+		FileChannel fromFileChannel = null;
+		FileChannel toFileChannel = null;
 		try {
 			RandomAccessFile randomAccessFile = new RandomAccessFile(fromFilePath, "r");
 			FileOutputStream fileOutputStream = new FileOutputStream(toFilePath);
 
-			FileChannel fromFileChannel = randomAccessFile.getChannel();
-			FileChannel toFileChannel = fileOutputStream.getChannel();
+			fromFileChannel = randomAccessFile.getChannel();
+			toFileChannel = fileOutputStream.getChannel();
 
 			//创建一个10kb的Buffer
 			ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 10);
-			
+
 			//将管道的数据写到Buffer里面
 			while (fromFileChannel.read(byteBuffer) != -1) {
 				//flip方法将Buffer从写模式切换到读模式
 				byteBuffer.flip();
-				
+
 				//将Buffer里的数据写到管道里面
 				toFileChannel.write(byteBuffer);
 
@@ -39,14 +41,23 @@ public class NioCopy {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (fromFileChannel != null)
+					fromFileChannel.close();
+				if (toFileChannel != null)
+					toFileChannel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
+
 		System.out.println("copy完成。");
 	}
 
 	public static void main(String[] args) {
 		NioCopy nioCopy = new NioCopy();
-		
+
 		nioCopy.copy("d:/Adobe_Photoshop_CS5_Extend_XiaZaiBa.zip", "d:/copy_1.zip");
 	}
 }
